8d6e0c1d220145bd35bf4105d462b0a776ea600e
vyagi <marcin.jagiela@hotmail.com>
2023-10-31 19:07:01 +0100

    Student done

---
+++ ProgrammingClasses/ObjectOrientedProgrammingIntroduction/OldSchoolStudent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingIntroduction
+{
+    class OldSchoolStudent //this is how it is done in Java world
+    {
+        private string Name;
+        private string LastName;
+        private int Age;
+
+        public OldSchoolStudent(string name, string lastName, int age)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Name cannot be empty");
+
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentNullException("LastName cannot be empty");
+
+            if (age < 16)
+                throw new ArgumentNullException("Student need to be not less than 16");
+
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetLastName()
+        {
+            return LastName;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public void SetAge(int newAge)
+        {
+            if (newAge < 16)
+                throw new ArgumentNullException("Student need to be not less than 16");
+            Age = newAge;
+        }
+
+        public void SetLastName(string newLastName)
+        {
+            if (string.IsNullOrWhiteSpace(newLastName))
+                throw new ArgumentNullException("LastName cannot be empty");
+            LastName = newLastName;
+        }
+    }
+}
--- ProgrammingClasses/ObjectOrientedProgrammingIntroduction/Program.cs
+++ ProgrammingClasses/ObjectOrientedProgrammingIntroduction/Program.cs
@@ -2,71 +2,74 @@
 
 namespace ObjectOrientedProgrammingIntroduction
 {
-    class Student
+    public class Student 
     {
-        private string Name;
-        private string LastName;
-        private int Age;
+        private string _name;
+        private string _lastName;
+        private int _age;
 
         public Student(string name, string lastName, int age)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentNullException("Name cannot be empty");
-            
+
             if (string.IsNullOrWhiteSpace(lastName))
                 throw new ArgumentNullException("LastName cannot be empty");
 
             if (age < 16)
                 throw new ArgumentNullException("Student need to be not less than 16");
 
-            Name = name;
-            LastName = lastName;
-            Age = age;
+            _name = name;
+            _lastName = lastName;
+            _age = age;
         }
 
-        public string GetName()
+        public string FirstName => _name;
+        public string LastName
         {
-            return Name;
+            get => _lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("LastName cannot be empty");
+
+                _lastName = value;
+            }
         }
 
-        public string GetLastName()
+        public int Age
         {
-            return LastName;
-        }
-
-        public int GetAge()
-        {
-            return Age;
-        }
-
-        public void SetAge(int newAge)
-        {
-            if (newAge < 16)
-                throw new ArgumentNullException("Student need to be not less than 16");
-            Age = newAge;
-        }
-
-        public void SetLastName(string newLastName)
-        {
-            if (string.IsNullOrWhiteSpace(newLastName))
-                throw new ArgumentNullException("LastName cannot be empty");
-            LastName = newLastName;
+            get => _age;
+            set
+            {
+                if (value < 16)
+                    throw new ArgumentNullException("Student need to be not less than 16");
+                _age = value;
+            }
         }
     }
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student("John", "Smith", 20);
-            
-            Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
+            //Java style
+            //OldSchoolStudent s1 = new OldSchoolStudent("John", "Smith", 20);
 
-            s1.SetAge(21);
-            Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
-            
-            s1.SetLastName("Bond");
-            Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
+            //Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
+
+            //s1.SetAge(21);
+            //Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
+
+            //s1.SetLastName("Bond");
+            //Console.WriteLine($"{s1.GetName()} {s1.GetLastName()} is {s1.GetAge()} years old");
+
+            //C# style
+            Student s1 = new Student("John", "Smith", 20);
+            s1.LastName = "Bond";
+            s1.Age = 18;
+            Console.WriteLine($"{s1.FirstName} {s1.LastName} is {s1.Age} years old");
         }
     }
 }
