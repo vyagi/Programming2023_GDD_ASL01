6c19e299bd9cfbd8d1c3d0bec706dc611ca9ec28
vyagi <marcin.jagiela@hotmail.com>
2023-10-31 18:23:40 +0100

    Initial commit

---
+++ ProgrammingClasses/ObjectOrientedProgrammingIntroduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
